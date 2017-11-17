c37089a9ff742f79b6139ee2ed625e5d8b578c1d
Dario Villanueva Ablanedo <dario@alolo.co>
2017-11-17 13:15:04 +0000

    import the public headers of the framework

--- Segment-Firebase-iOS/Segment_Firebase_iOS.h
+++ Segment-Firebase-iOS/Segment_Firebase_iOS.h
@@ -5,7 +5,6 @@
 //  Created by Dario Villanueva Ablanedo on 17/11/2017.
 //  Copyright © 2017 Feeld.co. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for Segment_Firebase_iOS.
@@ -15,5 +14,6 @@ FOUNDATION_EXPORT double Segment_Firebase_iOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char Segment_Firebase_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Segment_Firebase_iOS/PublicHeader.h>
-
+#import <Segment_Firebase_iOS/SEGFirebaseIntegration.h>
+#import <Segment_Firebase_iOS/SEGFirebaseIntegrationFactory.h>
 
