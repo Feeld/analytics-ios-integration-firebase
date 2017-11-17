1072e17ae1f149e57cdc4550c0f62bb73fb8a425
Dario Villanueva Ablanedo <dario@alolo.co>
2017-11-17 10:41:09 +0000

    Create shared scheme and xcodeproject

---
+++ Segment-Firebase-iOS/Segment-Firebase-iOS/Segment_Firebase_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Segment_Firebase_iOS.h
+//  Segment-Firebase-iOS
+//
+//  Created by Dario Villanueva Ablanedo on 17/11/2017.
+//  Copyright © 2017 Feeld.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Segment_Firebase_iOS.
+FOUNDATION_EXPORT double Segment_Firebase_iOSVersionNumber;
+
+//! Project version string for Segment_Firebase_iOS.
+FOUNDATION_EXPORT const unsigned char Segment_Firebase_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Segment_Firebase_iOS/PublicHeader.h>
+
+
